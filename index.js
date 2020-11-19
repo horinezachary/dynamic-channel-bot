6605531011edd44e10f81a8085c5aac718025448
Zachary Horine <zachary@horine.dev>
2020-11-18 21:10:12 -0800

    add base code for registering channels

---
+++ index.js
@@ -0,0 +1,78 @@
+const CLIENT_TOKEN = "NTY1MzUzMzc0MzI5OTk1Mjk2.XK1MXA.yUNzLtCxazzNoX3vkP7irOQiq5A";
+
+const dbCon = require('./sqlite_lib');
+dbCon.start();
+
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.login(CLIENT_TOKEN);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//add watched channel
+client.on('message', async message => {
+  console.log(message.content);
+  if (message.content.startsWith("z$register")) {
+    if (hasPermission(message.channel, message.author)) {
+      var channels = message.content.match(/(?=\s)?([0-9]{18})(?=\s)?/g); //returns array of channel ids in message
+      for (c of channels) {
+        console.log(c);
+        var taggedChannel = message.guild.channels.cache.get(c);
+        if (taggedChannel.type == "voice") {
+          registerChannel(taggedChannel);
+        }
+      }
+    }
+  }
+  if (message.content.startsWith("z$list")) {
+    var registered = await getRegistered();
+    var description = "```";
+    for (r of registered) {
+      console.log(r.name + " | " + r.id + ":" + r.guild + "\n");
+      description += r.name + " | " + r.id + ":" + r.guild + "\n";
+    }
+    embed("Registered Channels","FEFEFE",description+"```",message.channel);
+  }
+  if (message.content.startsWith("z$close")) {
+    dbCon.close();
+    process.exit();
+  }
+});
+
+function hasPermission(channel, author) {
+  //console.log(channel.guild);
+  return true;
+}
+
+async function registerChannel (channel) {
+    await dbCon.run(`INSERT INTO channels(channelId) VALUES(${channel.id})`);
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+}
+
+async function getRegistered() {
+  let returnedChannels = [];
+  let rows = await dbCon.query(`SELECT channelId as id FROM channels`);
+  console.log(rows);
+  for (row of rows) {
+    var channel = client.channels.cache.get(row.id);
+    var channelRow = {"id":row.id, "guild":channel.guild.id, "name":channel.name};
+    var len = returnedChannels.push(channelRow);
+    console.log(channelRow);
+  }
+  return returnedChannels;
+}
+
+function embed(title,color,description,channel) {
+  const embed = new discord.MessageEmbed()
+      // Set the title of the field
+      .setTitle(title)
+      // Set the color of the embed
+      .setColor(color)
+      // Set the main content of the embed
+      .setDescription(description);
+    // Send the embed to the same channel as the message
+    channel.send(embed);
+}
---
+++ sqlite_lib.js
@@ -0,0 +1,52 @@
+const sqlite3 = require('sqlite3').verbose();
+var db;
+function dbInit() {
+  db = new sqlite3.Database('./db/dynChannel.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the dynChannel SQlite database.');
+  });
+  db.run(`CREATE TABLE IF NOT EXISTS channels(channelId VARCHAR(18))`);
+}
+
+//returns data. query run as db.all
+function asyncQuery(query, values) {
+  return new Promise((resolve, reject) => {
+    db.all(query, values, function(error, results) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      resolve(results);
+    });
+  });
+}
+
+//does not need to return data.
+function asyncRun(query, values) {
+  return new Promise((resolve, reject) => {
+    db.run(query, values, function(error) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      resolve(results);
+    });
+  });
+}
+
+module.exports = {
+  start: function() {
+    return dbInit();
+  },
+  query: function(query, values) {
+    return asyncQuery(query, values);
+  },
+  run: function(query, values) {
+    return asyncRun(query, values);
+  },
+  close: function() {
+    db.close();
+  }
+}
